62d6a04afabeb18a376ec4f60fe60d4e38516d1b
remotetigertraining <153756546+remotetigertraining@users.noreply.github.com>
2024-02-23 11:39:57 -0500

    Initial commit

---
+++ src/main/java/com/rt/emp/empmicro/EmpMicroApplication.java
@@ -0,0 +1,13 @@
+package com.rt.emp.empmicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpMicroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rt/emp/empmicro/EmpMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rt.emp.empmicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpMicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
